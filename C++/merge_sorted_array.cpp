f571ada32e73d41129d9872686ef44cbb4ba0ae6
Anand Prakash <142045603+AnandPrakash21@users.noreply.github.com>
2024-03-06 22:47:50 +0530

    Update merge_sorted_array.cpp

--- C++/merge_sorted_array.cpp
+++ C++/merge_sorted_array.cpp
@@ -12,17 +12,3 @@ class Solution
         sort(nums1.begin(),nums1.end());
     }
 };
-int main()
-{
-    int  t;cin>>t;while(t--)
-    {
-            int a,b;
-            cin>>a>>b;
-            vector<int> v1(a),v2(b);
-            Solution obj;
-            obj.merge(v1,a-1,v2,b-1);
-            cout<<"Merged array is : ";
-            for(auto x:v1)cout<<x<<" ";
-            cout<<endl;
-    }
-}
