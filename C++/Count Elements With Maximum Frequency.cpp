e169674912b1b97d11790bc313572b9dfadee4d0
Anand Prakash <142045603+AnandPrakash21@users.noreply.github.com>
2024-03-08 07:18:58 +0530

    Update Count Elements With Maximum Frequency.cpp

--- C++/Count Elements With Maximum Frequency.cpp
+++ C++/Count Elements With Maximum Frequency.cpp
@@ -1,2 +1,27 @@
 #include<bits/stdc++.h>
-class 
+class Solution
+{
+public:
+int maxFrequencyElements(vector<int>& nums)
+{
+  map<int,int>mp;
+  int c=0;
+  int max=0;
+  for(int i = 0; i < nums.size(); i++)
+  {
+    mp[nums[i]]++;
+
+    if(mp[nums[i]] > max)
+    {
+      max = mp[nums[i]];
+      c=0;
+    }
+
+    if(mp[nums[i]] == max)
+    {
+      c++;
+    }
+  }
+  return c*max;
+}
+};
